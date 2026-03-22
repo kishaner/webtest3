574301682527464740102b0d3d6d22b2fa84a19e
Kishan Nathan <nathankishan65@gmail.com>
2026-03-22 05:16:59 -0500

    Initial commit

---
+++ wp-content/uploads/custom-css-js/116.js
@@ -0,0 +1,13 @@
+<!-- start Simple Custom CSS and JS -->
+<script type="text/javascript">
+/* Default comment here */ 
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  effect: 'coverflow',
+  coverflowEffect: {
+    rotate: 0,
+    slideShadows: true,
+  },
+});</script>
+<!-- end Simple Custom CSS and JS -->
